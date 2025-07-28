5b01d9578a57d698d28bd44be826477b1b43d9bc
JonasAndrade-20 <engmecjonas20@gmail.com>
2025-07-28 19:10:13 -0300

    criação de nova página para lubrificação de rolamentos

---
+++ Pages/rolamento.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import math
+
+st.set_page_config(page_icon="Lubrificação de rolamentos", page_icon="⚙️", layout="wide")
+
+st.title("Lubrificação de Rolamentos ⚙️")
+#st.link_button("Catálogo SKF", "https://www.skf.com/br/products/rolling-bearings/lubrication/index.html", "Catálogo SKF", icon="🔗")
