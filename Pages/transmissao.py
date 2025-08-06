d078fa4c3b59296d789dde804348579d5827f2f5
JonasAndrade-20 <engmecjonas20@gmail.com>
2025-08-05 21:43:48 -0300

    alteração das bibliotecas

--- Pages/transmissao.py
+++ Pages/transmissao.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import numpy as np
 from fractions import Fraction
-import matplotlib.pyplot as plt
+import plotly.express as px
 
 st.title("Relação de Transmissão - Correias")
 st.image("Pages/correiais_img.png", width=1500)
 
 st.write("Dados da Polia Motora:")
-d=st.number_input("Insira o Diâmetro da Polia Motora em mm")
-D=st.number_input("Insira o Diâmetro da Polia Movida em mm")
-n=st.number_input("Informe a rotação do motor em RPM")
+d=st.number_input("Insira o Diâmetro da Polia Motora em mm",min_value=1,value=100)
+D=st.number_input("Insira o Diâmetro da Polia Movida em mm",min_value=1,value=200)
+n=st.number_input("Informe a rotação do motor em RPM",min_value=1,value=1500)
 
 #Relação de transmissão
 #nd=ND -> N=nd/D
@@ -20,12 +20,11 @@ if st.button("Realizar Cálculo"):
     ratio_transm=Fraction(int(N),int(n))
     diametros=np.linspace(d,D,100)
     Ngrafico=n*d/diametros
-    st.success(f"A rotação da polia movida é de {N} RPM")
+    st.success(f"A rotação da polia movida é de {N:.2f} RPM")
     st.success(f"A relação de transmissão é de {ratio_transm}")
-    fig,ax = plt.subplots()
-    ax.plot(diametros,Ngrafico)
-    ax.set_xlabel("Diâmetro da polia movida (mm)")
-    ax.set_ylabel("Rotação da polia movida (RPM)")
-    ax.set_title("Relação da Rotação da Polia Mov. em função do diâmetro")
-    st.pyplot(fig)
+    fig=px.line(x=diametros,y=Ngrafico,labels={"x": "Diâmetro da polia movida (mm)",
+                "y": "Rotação da polia movida (RPM)"},
+                title="Relação da Rotação da Polia Movida em função do Diâmetro",
+                markers=True)
+    st.plotly_chart(fig)
     
